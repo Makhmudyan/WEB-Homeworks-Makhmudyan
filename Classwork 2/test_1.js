927984e0a4c260c7dc734f3279c4b17288875381
Marianna <makhmudyan@yandex.ru>
2023-09-12 14:49:10 +0400

    Classwork 2

---
+++ Classwork 2/test_1.js
@@ -0,0 +1,3 @@
+let number_one = parseInt(prompt("Введите первое число: "));
+let number_two = parseInt(prompt("Введите второе число: "));
+alert(`${number_one} + ${number_two} = ${number_one + number_two}`);
